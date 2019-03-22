acf8c8b70f911570e75645a60eb5be4013a70c23
liminlu <liminlu0314@163.com>
2019-03-22 16:33:02 +0800

    修改PROJ库到6.0

--- gdal/frmts/mbtiles/tfatmbtilesdataset.cpp
+++ gdal/frmts/mbtiles/tfatmbtilesdataset.cpp
@@ -710,8 +710,7 @@ const char* TfatMBTilesDataset::GetProjectionRef()
 {
 	// Global Geodetic tiles are using geodetic coordinates (latitude, longitude)
 	// directly as planar coordinates XY (it is also called Unprojected or Plate Carre). 
-	return SRS_WKT_WGS84;
-	//return "PROJCS[\"WGS 84 / Plate Carree\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Equirectangular\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",0],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AUTHORITY[\"EPSG\",\"32662\"]]";
+	return SRS_WKT_WGS84_LAT_LONG;
 }
 
 /************************************************************************/
